20a48465b005f62691d694076434289e2e8f294a
baarnacle <azd7@daum.net>
2021-12-13 11:07:05 +0900

    sky

--- SKY/8.flask-api-server/api/team_two.py
+++ SKY/8.flask-api-server/api/team_two.py
@@ -5,44 +5,64 @@ import json
 
 team_two = Blueprint('team_two', __name__)
 
-# @team_two.route("/")
-# def index():
-#     return "This is Api Service for Team Two"
+@team_two.route("/")
+def main():
+    return "This is Api Service for Team Two"
 
 @team_two.route("/recruit-score")
-def index():
+def recruit_senti_score():
     from modules import news_based_Senti
     news_based_Senti.naver_news_it_recruit("it_recruit_news.csv")
     total_score = news_based_Senti.calculate_score("it_recruit_news.csv", "/Users/sky/class_python/5.Sentiment_Analysis/senti_dict.csv")
-    return str(total_score)
+    
+    return "the senti score is {}".format(str(total_score))
+    
+@team_two.route("/crawler/jobkorea")
+def crawler_jobkorea():
+    from modules import recruit_crawler
+    recruit_crawler.scrape_jobkorea_recruitIT('recruit1.csv')
+    return ('Done')
 
-# @team_two.route("/crawler/jobkorea")
-# def index():
-#     from modules import recruit_crawler
-#     recruit_crawler.scrape_jobkorea_recruitIT('recruit1.csv')
-#     return ('finished')
+@team_two.route("/crawler/saramin")
+def crawler_saramin():
+    from modules import recruit_crawler
+    recruit_crawler.scrape_saramin_recruitIT('recruit2.csv')
+    return ('Done')
 
-# @team_two.route("/crawler/saramin")
-# def index():
-#     from modules import recruit_crawler
-#     recruit_crawler.scrape_saramin_recruitIT('recruit2.csv')
-#     return ('finished')
+@team_two.route("/crawler/rookie10j")
+def rookie_jobkorea():
+    from modules import recruit_rookie_hot10_crawler_
+    recruit_rookie_hot10_crawler_.scrape_jobkorea_rookie('rookie1.csv')
+    return ('Done')
 
-# @team_two.route("/crawler/rookie10/j")
-# def index():
-#     from modules import recruit_rookie_hot10_crawler_
-#     recruit_rookie_hot10_crawler_.scrape_jobkorea_rookie('rookie1.csv')
-#     return ('Done')
+@team_two.route("/crawler/rookie10s")
+def rookie_saramin():
+    from modules import recruit_rookie_hot10_crawler_
+    recruit_rookie_hot10_crawler_.scrape_saramin_rookie('rookie2.csv')
+    return ('Done')
 
-# @team_two.route("/crawler/rookie10/s")
-# def index():
-#     from modules import recruit_rookie_hot10_crawler_
-#     recruit_rookie_hot10_crawler_.scrape_saramin_rookie('rookie2.csv')
-#     return ('Done')
+@team_two.route('/itemrecommend')
+def item_recommend():
+    from modules import item_based_recommend
+     
+    simscores = item_based_recommend.item_based_recommend('/Users/sky/class_python/6.Recommendation/saramin_jobkorea_sjy_real.csv')
+    return str(simscores)
+
+@team_two.route('/cfrecommend')
+def collaborative_recommend():
+    from modules import collaborative_recommend
+    predict = collaborative_recommend.collaborative_recommend('/Users/sky/class_python/6.Recommendation/company_member.csv')
+    return str(predict)
+    
 
 
     
     
+#################################################################
+#################################################################
+#################################################################
+#################################################################
+#################################################################
 
 @team_two.route("/demo-one")
 def demo_one() :
